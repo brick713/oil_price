7a1c5d3266228235b1ce3056f961c2466c9f9bd0
brick713 <hibrick713@gmail.com>
2026-03-04 11:16:13 +0800

    fix bug

--- custom_components/oil_price/sensor.py
+++ custom_components/oil_price/sensor.py
@@ -175,7 +175,7 @@ class OilPriceDataCoordinator:
             
             time_match = re.search(r'油价\s*(\d{1,2}月\d{1,2}日)\s*(\d{1,2})\s*时\s*调整', hint_text)
             if not time_match:
-                time_match = re.search(r'油价\s*(\d{1,2}月\d{1,2}日)\s*(\d{1,2})\s*时, hint_text)
+                time_match = re.search(r'油价\s*(\d{1,2}月\d{1,2}日)\s*(\d{1,2})\s*时', hint_text)
             _LOGGER.debug("解析错误时间信息")
             if not time_match:
                 _LOGGER.debug("未找到时间信息")
@@ -203,7 +203,7 @@ class OilPriceDataCoordinator:
             
             price_match = re.search(r'(上调|上涨|下跌)([\d.]+元/升-[\d.]+元/升)', hint_text)
             if not price_match:
-                _LOGGER.debug("解析错误价格信息")
+                _LOGGER.debug("解析错误时间信息")
                 return
             
             direction = price_match.group(1)
